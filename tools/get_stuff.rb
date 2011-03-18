4aac18e6777959234426848535df70dcba912f86
Peter Hoeg <peter@hoeg.com>
2011-03-18 16:23:26 +0800

    a few more steps

--- tools/get_stuff.rb
+++ tools/get_stuff.rb
@@ -1,29 +1,57 @@
 #!/usr/bin/env ruby
 
+require 'uri'
+require 'open-uri'
+
+BIT_32_DIR="x86"
+BIT_64_DIR="amd64"
+
+class AppPackage
+
+end
+
+class AppPackageGroup
+
+  def initialize(name, directory, packages)
+    [ packages ].flatten.each do |p|
+    end
+  end
+
+end
+
+class AppPackageHandler
+
+end
+
 FLASH = [
-  Hash.new (:url => "http://www.adobe.com/go/full_flashplayer_win_msi", 
-            :file => 'install_flash_player_10_active_x.msi'),
-  Hash.new (:url => "http://www.adobe.com/go/full_flashplayer_win_pl_msi", 
-            :file => 'install_flash_player_10_plugin.msi'),
+  { :url => "http://www.adobe.com/go/full_flashplayer_win_msi",
+            :file => 'install_flash_player_10_active_x.msi'},
+  {:url => "http://www.adobe.com/go/full_flashplayer_win_pl_msi",
+            :file => 'install_flash_player_10_plugin.msi'},
   ]
 
 VJ = [
-  Hash.new (:url => "http://download.microsoft.com/download/f/1/7/f175de5b-e7af-4231-9a65-417611bbedfe/vjredist64.exe",
-            :file => "amd64/vjredist.exe"),
-  Hash.new (:url => "http://download.microsoft.com/download/9/2/3/92338cd0-759f-4815-8981-24b437be74ef/vjredist.exe",
-            :file => "x86/vjredist.exe")
+  {:url => "http://download.microsoft.com/download/f/1/7/f175de5b-e7af-4231-9a65-417611bbedfe/vjredist64.exe",
+            :file => "amd64/vjredist.exe"},
+  {:url => "http://download.microsoft.com/download/9/2/3/92338cd0-759f-4815-8981-24b437be74ef/vjredist.exe",
+            :file => "x86/vjredist.exe"}
   ]
 
 IE9 = [
-  Hash.new (:url => "http://download.microsoft.com/download/C/3/B/C3BF2EF4-E764-430C-BDCE-479F2142FC81/IE9-Windows7-x86-enu.exe",
-            :file => "x86/IE9-Windows7-enu.exe"),
-  Hash.new (:url => "http://download.microsoft.com/download/C/1/6/C167B427-722E-4665-9A40-A37BC5222B0A/IE9-Windows7-x64-enu.exe",
-            :file => "amd64/IE9-Windows7-enu.exe"),
+  {:url => "http://download.microsoft.com/download/C/3/B/C3BF2EF4-E764-430C-BDCE-479F2142FC81/IE9-Windows7-x86-enu.exe",
+            :file => "x86/IE9-Windows7-enu.exe"},
+  {:url => "http://download.microsoft.com/download/C/1/6/C167B427-722E-4665-9A40-A37BC5222B0A/IE9-Windows7-x64-enu.exe",
+            :file => "amd64/IE9-Windows7-enu.exe"},
   ]
 
 LASTPASS = [
-  Hash.new (:url => "https://lastpass.com/lastpass.exe",
-            :file => "x86/lastpass.exe"),
-  Hash.new (:url => "https://lastpass.com/lastpass_x64.exe",
-            :file => "amd64/lastpass.exe"),
+  {:url => "https://lastpass.com/lastpass.exe",
+            :file => "x86/lastpass.exe"},
+  {:url => "https://lastpass.com/lastpass_x64.exe",
+            :file => "amd64/lastpass.exe"},
   ]
+
+
+LASTPASS.each do |p|
+  system "wget -c #{p[:url]} -O #{p[:file]}"
+end
