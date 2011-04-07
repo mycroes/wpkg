3bfd5563ae0333fcb593d6f4e9b698b508d07ad9
Peter Hoeg <peter@hoeg.com>
2011-04-07 13:01:33 +0800

    change: also check rb files. YES, I know we should make proper tests instead

--- clean_up.sh
+++ clean_up.sh
@@ -3,10 +3,11 @@ find . -type f -name '*~' -delete
 test -x fromdos && fromdos *.xml
 xml=$(which xmllint)
 if [ -x ${xml} ] ; then
-  ${xml} --noout *.xml
+  find . -name '*.xml' -print0 | xargs -0 ${xml} --noout
 else
   echo xmllint not found - please use some other XML checker to validate...
   exit 1
 fi
 find . -type f ! -name clean_up.sh -print0 | xargs -0 chmod 644
 find . \( -name '*.xml' -o -name '*.rb' \) -print0 | xargs -0 sed -i 's/[ \t]\+$//g'
+find . -name '*.rb' -print0 | xargs -0 ruby -c
