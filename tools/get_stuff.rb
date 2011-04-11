fab68123add3fc615ca0f5cc52f731bc5622ed41
Peter Hoeg <peter@hoeg.com>
2011-04-11 09:29:46 +0800

    change: turn definitions into a hash to remove a lot of option handling

--- tools/get_stuff.rb
+++ tools/get_stuff.rb
@@ -7,47 +7,60 @@ require 'pathname'
 BIT_32_DIR="x86"
 BIT_64_DIR="amd64"
 
-FLASH = [
+PACKAGES = {
+
+:dotnet3 => [
+  {:url => 'http://download.microsoft.com/download/2/0/e/20e90413-712f-438c-988e-fdaa79a8ac3d/dotnetfx35.exe',
+            :file => File.join('Microsoft', 'DotNet', 'dotnetfx35.exe')},
+  {:url => 'http://download.microsoft.com/download/C/6/A/C6ADC586-2518-404B-8973-E1E22C556AF4/NDP20SP2-KB958481-x86.exe',
+            :file => File.join('Microsoft', 'DotNet', 'NDP20SP2-KB958481-x86.exe')},
+  {:url => 'http://download.microsoft.com/download/C/6/A/C6ADC586-2518-404B-8973-E1E22C556AF4/NDP30SP2-KB958483-x86.exe',
+            :file => File.join('Microsoft', 'DotNet', 'NDP30SP2-KB958483-x86.exe')},
+  {:url => 'http://download.microsoft.com/download/C/6/A/C6ADC586-2518-404B-8973-E1E22C556AF4/NDP35SP1-KB958484-x86.exe',
+            :file => File.join('Microsoft', 'DotNet', 'NDP35SP1-KB958484-x86.exe')},
+  ],
+
+:flash => [
   {:url => 'http://www.adobe.com/go/full_flashplayer_win_msi',
             :file => File.join('Adobe', 'Flash', 'install_flash_player_10_active_x.msi')},
   {:url => 'http://www.adobe.com/go/full_flashplayer_win_pl_msi',
             :file => File.join('Adobe', 'Flash', 'install_flash_player_10_plugin.msi')},
-  ]
+  ],
 
-IE9 = [
+:ie9 => [
   {:url => 'http://download.microsoft.com/download/C/3/B/C3BF2EF4-E764-430C-BDCE-479F2142FC81/IE9-Windows7-x86-enu.exe',
             :file => File.join('Microsoft', 'IE9', BIT_32_DIR, 'IE9-Windows7-enu.exe')},
   {:url => 'http://download.microsoft.com/download/C/1/6/C167B427-722E-4665-9A40-A37BC5222B0A/IE9-Windows7-x64-enu.exe',
             :file => File.join('Microsoft', 'IE9', BIT_64_DIR, 'IE9-Windows7-enu.exe')},
-  ]
+  ],
 
-INSTALLER = [
+:installer => [
   {:url => 'http://download.microsoft.com/download/5/f/d/5fdc6240-2127-42b6-8e16-bab6171db233/WindowsXP-KB898461-x86-ENU.exe',
             :file => File.join('Microsoft', 'Windows Installer', 'WindowsXP-KB898461-x86-ENU.exe')},
   {:url => 'http://download.microsoft.com/download/2/6/1/261fca42-22c0-4f91-9451-0e0f2e08356d/WindowsXP-KB942288-v3-x86.exe',
             :file => File.join('Microsoft', 'Windows Installer', 'WindowsXP-KB942288-v3-x86.exe')},
-  ]
+  ],
 
-LASTPASS = [
+:lastpass => [
   {:url => 'https://lastpass.com/lastpass.exe',
             :file => File.join('LastPass', BIT_32_DIR, 'lastpass.exe')},
   {:url => 'https://lastpass.com/lastpass_x64.exe',
             :file => File.join('LastPass', BIT_64_DIR, 'lastpass.exe')},
-  ]
+  ],
 
-SKYPE = [
+:skype => [
   {:url => 'http://download.skype.com/SkypeSetup.msi',
             :file => File.join('Skype', 'SkypeSetup.msi')},
-  ]
+  ],
 
-TRUEVIEW = [
+:trueview => [
   {:url => 'http://download.autodesk.com/esd/dwgtrueview/2011/SetupDWGTrueView2011_32bit.exe',
             :file => File.join('Autodesk', 'DWGTureview', '2011', BIT_32_DIR, 'SetupDWGTrueView2011_32bit.exe')},
   {:url => 'http://download.autodesk.com/esd/dwgtrueview/2011/SetupDWGTrueView2011_64bit.exe',
             :file => File.join('Autodesk', 'DWGTureview', '2011', BIT_64_DIR, 'SetupDWGTrueView2011_64bit.exe')},
-  ]
+  ],
 
-VC = [
+:vc => [
   #VC6
   {:url => 'http://download.microsoft.com/download/vc60pro/update/1/w9xnt4/en-us/vc6redistsetup_enu.exe',
             :file => File.join('Microsoft', 'VCRuntime', BIT_32_DIR, 'vcredist_6.exe')},
@@ -67,25 +80,26 @@ VC = [
   {:url => 'http://download.microsoft.com/download/3/2/2/3224B87F-CFA0-4E70-BDA3-3DE650EFEBA5/vcredist_x64.exe',
             :file => File.join('Microsoft', 'VCRuntime', BIT_64_DIR, 'vcredist_2010.exe')},
 
-  ]
+  ],
 
-VJ = [
+:vj => [
   {:url => 'http://download.microsoft.com/download/9/2/3/92338cd0-759f-4815-8981-24b437be74ef/vjredist.exe',
             :file => File.join('Microsoft', 'VisualJ', BIT_32_DIR, 'vjredist.exe')},
   {:url => 'http://download.microsoft.com/download/f/1/7/f175de5b-e7af-4231-9a65-417611bbedfe/vjredist64.exe',
             :file => File.join('Microsoft', 'VisualJ', BIT_64_DIR, 'vjredist.exe')},
-  ]
+  ],
 
-VLC = [
+:vlc => [
   {:url => 'http://sourceforge.net/projects/vlc/files/1.1.8/win32/vlc-1.1.8-win32.exe/download',
             :file => File.join('VLC', 'vlc-1.1.8-win32.exe')},
-  ]
+  ],
+  
+}
 
 opts = GetoptLong.new(
   [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
   [ '--force', '-f', GetoptLong::NO_ARGUMENT ],
-  [ '--32', GetoptLong::NO_ARGUMENT ],
-  [ '--64', GetoptLong::NO_ARGUMENT ],
+  [ '--bits', '-b', GetoptLong::REQUIRED_ARGUMENT ],
   [ '--package', '-p', GetoptLong::REQUIRED_ARGUMENT ],
   )
 
@@ -105,10 +119,14 @@ get_stuff.rb [OPTIONS] --package PACKAGE_NAME DIR
 
   --force, -f:
     Force overwriting existing files
+    
+  --bits, -b 32|64:
+    Only download 32 or 64 bit packages
 
   --package, -p PACKAGE_NAME:
     Get one of the following packages:
       all	All packages
+      dotnet3	.NET 3.5
       flash	Flash Player
       ie9	Internet Explorer 9
       installer	Windows Installer 4.5
@@ -131,27 +149,9 @@ EOF
     when '--package','-p'
       package = case arg
 	when 'all'
-	  [ FLASH, IE9, INSTALLER, LASTPASS, SKYPE, TRUEVIEW, VC, VJ, VLC ]
-	when 'flash'
-	  FLASH
-	when 'ie9'
-	  IE9
-	when 'installer'
-	  INSTALLER
-	when 'lastpass'
-	  LASTPASS
-	when 'skype'
-	  SKYPE
-	when 'trueview'
-	  TRUEVIEW
-	when 'vc'
-	  VC
-	when 'vj'
-	  VJ
-	when 'vlc'
-	  VLC
-        else
-	  nil
+	  PACKAGES
+	else
+	  PACKAGES[arg.to_sym]
       end
   end
 end
@@ -164,16 +164,26 @@ else
   raise IOError, 'Destination directory not found' unless Dir.exists?(dir)
 end
 
-[ package ].flatten.each do |p|
-  path = File.join(dir, Pathname.new(p[:file]).dirname)
-  file = Pathname.new(p[:file]).basename
-  fullpath = File.join(path, file)
-  FileUtils.mkdir_p(path) unless Dir.exists?(path)
-  if File.exists?(fullpath)
-    if overwrite
-      FileUtils.safe_unlink(fullpath)
-      puts 'Removing existing file'
+def download(package_def, dir, overwrite)
+  package_def.each do |p|
+    path = File.join(dir, Pathname.new(p[:file]).dirname)
+    file = Pathname.new(p[:file]).basename
+    fullpath = File.join(path, file)
+    FileUtils.mkdir_p(path) unless Dir.exists?(path)
+    if File.exists?(fullpath)
+      if overwrite
+	FileUtils.safe_unlink(fullpath)
+	puts 'Removing existing file'
+      end
     end
+    system "axel #{p[:url]} -a -o #{fullpath}"
   end
-  system "axel #{p[:url]} -a -o #{fullpath}"
+end
+
+if package.is_a? Hash
+  package.each_pair do |k,v|
+    download(v, dir, overwrite)
+  end
+else
+  download(package, dir, overwrite)
 end
