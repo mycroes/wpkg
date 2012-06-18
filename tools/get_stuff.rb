8c2bdf0d1d9e7b7ae46a850898bcbba8c6b60e72
Peter Hoeg <peter@speartail.com>
2012-06-18 12:49:35 +0800

    fix for flash 11.3

--- tools/get_stuff.rb
+++ tools/get_stuff.rb
@@ -95,18 +95,13 @@ PACKAGES = {
   :language => 'en-US'},
 ],
 
+
 :flash => [
-  {:url => 'http://fpdownload.macromedia.com/pub/flashplayer/current/licensing/win/install_flash_player_%mainversion%_active_x_32bit.msi',
-  :destination => ['Adobe', 'Flash', '%shortversion%', BIT_32_DIR, 'install_flash_player_%mainversion%_active_x.msi'],
+  {:url => 'http://download.macromedia.com/get/flashplayer/current/licensing/win/install_flash_player_%mainversion%_active_x.msi',
+  :destination => ['Adobe', 'Flash', '%shortversion%', 'install_flash_player_%mainversion%_active_x.msi'],
   :package_id => 'flash' },
-  {:url => 'http://fpdownload.macromedia.com/pub/flashplayer/current/licensing/win/install_flash_player_%mainversion%_plugin_32bit.msi',
-  :destination => ['Adobe', 'Flash', '%shortversion%', BIT_32_DIR, 'install_flash_player_%mainversion%_plugin.msi'],
-  :package_id => 'flash' },
-  {:url => 'http://fpdownload.macromedia.com/pub/flashplayer/current/licensing/win/install_flash_player_%mainversion%_active_x_64bit.msi',
-  :destination => ['Adobe', 'Flash', '%shortversion%', BIT_64_DIR, 'install_flash_player_%mainversion%_active_x.msi'],
-  :package_id => 'flash' },
-  {:url => 'http://fpdownload.macromedia.com/pub/flashplayer/current/licensing/win/install_flash_player_%mainversion%_plugin_64bit.msi',
-  :destination => ['Adobe', 'Flash', '%shortversion%', BIT_64_DIR, 'install_flash_player_%mainversion%_plugin.msi'],
+  {:url => 'http://download.macromedia.com/get/flashplayer/current/licensing/win/install_flash_player_%mainversion%_plugin.msi',
+  :destination => ['Adobe', 'Flash', '%shortversion%', 'install_flash_player_%mainversion%_plugin.msi'],
   :package_id => 'flash' },
 ],
 
